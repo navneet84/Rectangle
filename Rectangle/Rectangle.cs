514c8f9ec5ee6514f58b5480657da08277c25e74
Ndhiman9008 <ndhiman9008@conestogac.on.ca>
2020-02-06 20:48:42 -0500

    Add project files.

---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangle
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+
+        }
+
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+
+        public void SetWidth(int width)
+        {
+            this.width = width;
+        }
+
+        public int GetWidth()
+        {
+            return this.width;
+        }
+
+    }
+}
